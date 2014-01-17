43a48795c48226738682a19ddceb2b9c29d44347
Martin Konrad <konrad@frib.msu.edu>
2014-01-17 15:50:05 -0500

    Make puppet-lint happy. This allows to use a pre-commit hook like https://github.com/drwahl/puppet-git-hooks

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,14 +25,14 @@ node 'archappl0.example.com' {
   include vagrant
 
   class { 'archiver_appliance::node':
-    nodes_fqdn		=> $archiver_nodes,
-    loadbalancer	=> $loadbalancer,
-    archappl_tarball_url	=> $archappl_tarball_url,
-    archappl_tarball_md5sum	=> $archappl_tarball_md5sum,
-    mysqlconnector_tarball_url	=> $mysqlconnector_tarball_url,
-    mysqlconnector_tarball_md5sum	=> $mysqlconnector_tarball_md5sum,
-    tomcatjdbc_tarball_url	=> $tomcatjdbc_tarball_url,
-    tomcatjdbc_tarball_md5sum	=> $tomcatjdbc_tarball_md5sum,
+    nodes_fqdn                    => $archiver_nodes,
+    loadbalancer                  => $loadbalancer,
+    archappl_tarball_url          => $archappl_tarball_url,
+    archappl_tarball_md5sum       => $archappl_tarball_md5sum,
+    mysqlconnector_tarball_url    => $mysqlconnector_tarball_url,
+    mysqlconnector_tarball_md5sum => $mysqlconnector_tarball_md5sum,
+    tomcatjdbc_tarball_url        => $tomcatjdbc_tarball_url,
+    tomcatjdbc_tarball_md5sum     => $tomcatjdbc_tarball_md5sum,
   }
 
   Class['vagrant'] -> Class['archiver_appliance::node']
@@ -42,12 +42,12 @@ node 'archappl1.example.com' {
   include vagrant
 
   class { 'archiver_appliance::node':
-    nodes_fqdn		=> $archiver_nodes,
-    loadbalancer	=> $loadbalancer,
-    archappl_tarball_url	=> $archappl_tarball_url,
-    archappl_tarball_md5sum	=> $archappl_tarball_md5sum,
-    mysqlconnector_tarball_url	=> $mysqlconnector_tarball_url,
-    mysqlconnector_tarball_md5sum	=> $mysqlconnector_tarball_md5sum,
+    nodes_fqdn                    => $archiver_nodes,
+    loadbalancer                  => $loadbalancer,
+    archappl_tarball_url          => $archappl_tarball_url,
+    archappl_tarball_md5sum       => $archappl_tarball_md5sum,
+    mysqlconnector_tarball_url    => $mysqlconnector_tarball_url,
+    mysqlconnector_tarball_md5sum => $mysqlconnector_tarball_md5sum,
   }
 
   Class['vagrant'] -> Class['archiver_appliance::node']
@@ -57,8 +57,8 @@ node 'archappl2.example.com' {
   include vagrant
 
   class { 'archiver_appliance::node':
-    nodes_fqdn		=> $archiver_nodes,
-    loadbalancer	=> $loadbalancer,
+    nodes_fqdn   => $archiver_nodes,
+    loadbalancer => $loadbalancer,
   }
 
   Class['vagrant'] -> Class['archiver_appliance::node']
@@ -77,115 +77,115 @@ node 'testioc.example.com' {
   }
 
   class { 'epics_softioc':
-    iocbase	=> $iocbase,
+    iocbase => $iocbase,
   }
 
-  file { "$iocbase/control":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/control',
-    recurse	=> true,
-    owner	=> 'root',
-    group	=> 'softioc',
-    notify	=> Epics_softioc::Ioc['control'],
+  file { "${iocbase}/control":
+    source  => '/vagrant/files/pvmanager/pvmanager-integration/epics/control',
+    recurse => true,
+    owner   => 'root',
+    group   => 'softioc',
+    notify  => Epics_softioc::Ioc['control'],
   }
 
-  file { "$iocbase/control/st.cmd":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/control/st.cmd',
-    owner	=> 'root',
-    group	=> 'softioc',
-    mode	=> '0755',
-    notify	=> Epics_softioc::Ioc['control'],
+  file { "${iocbase}/control/st.cmd":
+    source => '/vagrant/files/pvmanager/pvmanager-integration/epics/control/st.cmd',
+    owner  => 'root',
+    group  => 'softioc',
+    mode   => '0755',
+    notify => Epics_softioc::Ioc['control'],
   }
 
   epics_softioc::ioc { 'control':
-    bootdir	=> '',
-    consolePort	=> '4051',
-    enable	=> true,
-    ensure	=> running,
-    require	=> File["$iocbase/control"],
+    ensure      => running,
+    bootdir     => '',
+    consolePort => '4051',
+    enable      => true,
+    require     => File["${iocbase}/control"],
   }
 
   file { '/usr/local/bin':
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/bin',
-    recurse	=> true,
-    owner	=> 'root',
-    group	=> 'root',
+    source  => '/vagrant/files/pvmanager/pvmanager-integration/epics/bin',
+    recurse => true,
+    owner   => 'root',
+    group   => 'root',
   }
 
   file { '/etc/init.d/testcontroller':
-    source	=> '/vagrant/files/init.d/testcontroller',
-    owner	=> 'root',
-    group	=> 'root',
-    mode	=> '0755',
+    source => '/vagrant/files/init.d/testcontroller',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
 
   service { 'testcontroller':
-    ensure	=> running,
-    enable	=> true,
+    ensure => running,
+    enable => true,
   }
 
-  file { "$iocbase/phase1":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/phase1',
-    recurse	=> true,
-    owner	=> 'root',
-    group	=> 'softioc',
-    notify	=> Epics_softioc::Ioc['phase1'],
+  file { "${iocbase}/phase1":
+    source  => '/vagrant/files/pvmanager/pvmanager-integration/epics/phase1',
+    recurse => true,
+    owner   => 'root',
+    group   => 'softioc',
+    notify  => Epics_softioc::Ioc['phase1'],
   }
 
-  file { "$iocbase/phase1/st.cmd":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/phase1/st.cmd',
-    owner	=> 'root',
-    group	=> 'softioc',
-    mode	=> '0755',
-    notify	=> Epics_softioc::Ioc['phase1'],
+  file { "${iocbase}/phase1/st.cmd":
+    source => '/vagrant/files/pvmanager/pvmanager-integration/epics/phase1/st.cmd',
+    owner  => 'root',
+    group  => 'softioc',
+    mode   => '0755',
+    notify => Epics_softioc::Ioc['phase1'],
   }
 
   epics_softioc::ioc { 'phase1':
-    bootdir	=> '',
-    consolePort	=> '4053',
-    enable	=> false,
-    require	=> File["$iocbase/phase1"],
+    bootdir     => '',
+    consolePort => '4053',
+    enable      => false,
+    require     => File["${iocbase}/phase1"],
   }
 
   file { '/usr/local/lib/iocapps/typeChange1':
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange1',
-    recurse	=> true,
-    notify	=> Epics_softioc::Ioc['typeChange1'],
+    source  => '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange1',
+    recurse => true,
+    notify  => Epics_softioc::Ioc['typeChange1'],
   }
 
-  file { "$iocbase/typeChange1/st.cmd":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange1/st.cmd',
-    owner	=> 'root',
-    group	=> 'softioc',
-    mode	=> '0755',
-    notify	=> Epics_softioc::Ioc['typeChange1'],
+  file { "${iocbase}/typeChange1/st.cmd":
+    source => '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange1/st.cmd',
+    owner  => 'root',
+    group  => 'softioc',
+    mode   => '0755',
+    notify => Epics_softioc::Ioc['typeChange1'],
   }
 
   epics_softioc::ioc { 'typeChange1':
-    bootdir	=> '',
-    consolePort	=> '4053',
-    enable	=> false,
-    require	=> File["$iocbase/typeChange1"],
+    bootdir     => '',
+    consolePort => '4053',
+    enable      => false,
+    require     => File["${iocbase}/typeChange1"],
   }
 
-  file { "$iocbase/typeChange2":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange2',
-    recurse	=> true,
-    notify	=> Epics_softioc::Ioc['typeChange2'],
+  file { "${iocbase}/typeChange2":
+    source  => '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange2',
+    recurse => true,
+    notify  => Epics_softioc::Ioc['typeChange2'],
   }
 
-  file { "$iocbase/typeChange2/st.cmd":
-    source	=> '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange2/st.cmd',
-    owner	=> 'root',
-    group	=> 'softioc',
-    mode	=> '0755',
-    notify	=> Epics_softioc::Ioc['typeChange2'],
+  file { "${iocbase}/typeChange2/st.cmd":
+    source => '/vagrant/files/pvmanager/pvmanager-integration/epics/typeChange2/st.cmd',
+    owner  => 'root',
+    group  => 'softioc',
+    mode   => '0755',
+    notify => Epics_softioc::Ioc['typeChange2'],
   }
 
   epics_softioc::ioc { 'typeChange2':
-    bootdir	=> '',
-    consolePort	=> '4053',
-    enable	=> false,
-    require	=> File["$iocbase/typeChange2"],
+    bootdir     => '',
+    consolePort => '4053',
+    enable      => false,
+    require     => File["${iocbase}/typeChange2"],
   }
 
   Class['vagrant'] -> Class['apt']
@@ -207,40 +207,40 @@ node 'archiveviewer.example.com' {
   }
 
   package { 'task-lxde-desktop':
-    ensure	=> installed,
+    ensure => installed,
   }
 
   package { 'openjdk-7-jdk':
-    ensure	=> installed,
+    ensure => installed,
   }
 
   exec { 'use openjdk 7 by default':
-    command	=> '/usr/sbin/update-java-alternatives -s java-1.7.0-openjdk-amd64',
-    require	=> Package['openjdk-7-jdk'],
+    command => '/usr/sbin/update-java-alternatives -s java-1.7.0-openjdk-amd64',
+    require => Package['openjdk-7-jdk'],
   }
 
   package { 'epics-catools':
-    ensure	=> installed,
-    require	=> Apt::Source['nsls2repo'],
+    ensure  => installed,
+    require => Apt::Source['nsls2repo'],
   }
 
   wget::fetch { 'archiveviewer':
-    source	=> 'http://downloads.sourceforge.net/project/epicsarchiverap/snapshots/archiveviewer.jar',
-    destination	=> '/usr/local/lib/archiveviewer.jar',
+    source      => 'http://downloads.sourceforge.net/project/epicsarchiverap/snapshots/archiveviewer.jar',
+    destination => '/usr/local/lib/archiveviewer.jar',
   }
 
   file { '/usr/local/bin/archiveviewer.sh':
-    ensure	=> file,
-    source	=> '/vagrant/files/archiveviewer.sh',
-    owner	=> root,
-    mode	=> '0755',
-    require	=> Wget::Fetch['archiveviewer'],
+    ensure  => file,
+    source  => '/vagrant/files/archiveviewer.sh',
+    owner   => root,
+    mode    => '0755',
+    require => Wget::Fetch['archiveviewer'],
   }
 
   file { '/usr/share/applications/archiveviewer.desktop':
-    ensure	=> file,
-    source	=> '/vagrant/files/archiveviewer',
-    require	=> File['/usr/local/bin/archiveviewer.sh'],
+    ensure  => file,
+    source  => '/vagrant/files/archiveviewer',
+    require => File['/usr/local/bin/archiveviewer.sh'],
   }
 
   Class['vagrant'] -> Package['task-lxde-desktop']
@@ -252,7 +252,7 @@ node 'loadbalancer.example.com' {
   include vagrant
 
   class { 'archiver_appliance::loadbalancer':
-    nodes_fqdn	=> $archiver_nodes,
+    nodes_fqdn => $archiver_nodes,
   }
 
   Class['vagrant'] -> Class['archiver_appliance::loadbalancer']
